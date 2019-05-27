82f35d7c9b83c2c3875e8b2b1f733b282b73de0f
Aisa <aisajemila@gmail.com>
2019-05-27 11:18:14 +0300

    Created view for the home page. Included registration urls in the urls.py.

--- pro_jects/views.py
+++ pro_jects/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def home(request):
+  render(request,'home.html')
